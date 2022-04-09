44f85f5a45c4b562fc142cc66ec12a433b0c4f39
k2u6m5i3r <wunder49@yandex.by>
2022-04-09 21:00:33 +0300

    header, baner, slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 //here will by JavaScript code ...
+console.log("in script");
+$(function(){
+    $('.top__slider').slick({
+        arrows:false,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        fade:true,
+    });
+});
