0198be7c9142ed06c6ff6c507f1b7c314ccb9274
k2u6m5i3r <wunder49@yandex.by>
2022-04-15 17:27:16 +0300

    full+adaptiv

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,31 @@ $(function(){
         // autoplay:true,
         // autoplaySpeed:1000,
         fade:true,
+        responsive: [
+            {
+              breakpoint: 601,
+              settings: {
+
+                dots: false,
+              }
+            },
+            
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
+
+    $('.menu__btn').click(function (){
+        $('.header__menu-list').slideToggle();
+    });
+
+    // $(".top__to-go").on("click", "a", function (event){
+    //     event.preventDefault();
+    //     let id  = $(this).attr('href');
+
+    //     top = $(id).offset().top;
+
+    //     $('body, html').animate({scrollTop: top}, 900);
+    // });
 });
