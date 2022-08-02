b7b283f5846567319358e93f49d5c5bbf1f94e8e
k2u6m5i3r <wunder49@yandex.by>
2022-08-02 22:41:29 +0300

    Приветствие c примера

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,8 @@ function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
-    showDate();
+    showDate();// обновление даты для полуночи
+    showGreeting();// обновить время приветствия!
     setTimeout(showTime, 1000);
 }
 showTime();
@@ -20,3 +21,42 @@ showTime();
 }
 showDate();
 //END work for time&date
+// Приветствие
+
+const nameUserBlock = document.querySelector('.name');        //имя пользователя
+function showGreeting(){
+  const greetingBlock = document.querySelector('.greeting');  //получаю блок с временим суток
+  greetingBlock.textContent = `Good ${getTimeOfDay()}`;
+}
+
+function getTimeOfDay(){
+  const date = new Date();
+  const hours = date.getHours();
+  let ans="";
+  if(hours >= 6 && hours < 12){
+    ans = "morning";
+  }
+  if(hours >= 12 && hours < 18){
+    ans = "afternoon";
+  }
+  if(hours >= 18 && hours < 24){
+    ans = "evening"; 
+  }
+  if(hours >= 0 && hours < 6){
+    ans = "night"; 
+  }
+  return ans;
+}
+
+
+function setLocalStorage() {
+  localStorage.setItem('name', nameUserBlock.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    nameUserBlock.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage)
+// END приветствие
